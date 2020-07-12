5cbd19bce44a6ec1392a8731ea9b745478aaf29e
Rohan Jaiswal <rohan.jaiswal@freecharge.com>
2020-07-12 20:47:40 +0530

    Add models
    adding models for analytics

--- app/models/analytics.go
+++ app/models/analytics.go
@@ -5,11 +5,8 @@ import "time"
 // AdAnalyticsSchema mongo schema
 type AdAnalyticsSchema struct {
 	CreatedAt   time.Time `bson:"createdAt" json:"createdAt"`
-	UpdateAt    time.Time `bson:"updatedAt" json:"updatedAt"`
-	ExpireAt    time.Time `bson:"expireAt" json:"expireAt"`
 	ProductName string    `bson:"productName" json:"productName"`
-	RequestURL  string    `bson:"requestURL" json:"requestURL"`
-	RedirectURL string    `bson:"redirectURL" json:"redirectURL"`
+	URLHash     string    `bson:"requestURL" json:"requestURL"`
 	UserAgent   string    `bson:"userAgent" json:"userAgent"`
 	IP          string    `bson:"ipAddress" json:"ipAddress"`
 }
--- app/models/data.go
+++ app/models/data.go
@@ -1,1 +1,16 @@
 package models
+
+import "time"
+
+// URLData mongo schema
+type URLData struct {
+	CreatedAt   time.Time `bson:"expireAt" json:"createdAt"`
+	UpdateAt    time.Time `bson:"expireAt" json:"updateAt"`
+	ExpireAt    time.Time `bson:"expireAt" json:"expireAt"`
+	URLHash     string    `bson:"urlHash" json:"urlHash"`
+	URLOriginal string    `bson:"urlOriginal" json:"urlOriginal"`
+	IsWebToWeb  bool      `bson:"isWebToWeb" json:"isWebToWeb"`
+	IsWebToMob  bool      `bson:"isWebToMob" json:"isWebToMob"`
+	IsMobToApp  bool      `bson:"isMobToApp" json:"isMobToApp"`
+	IsAppToApp  bool      `bson:"isAppToApp" json:"isAppToApp"`
+}
